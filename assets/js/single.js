7d30fa949e6c1bb683a0b8fab61bac50bb5f5459
Max Kottong <max.kottong@gmail.com>
2022-07-07 14:08:14 -0400

    Added linking to other html page, and made the repo list dynamic

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,4 +1,18 @@
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
+
+var getRepoName = function () {
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+
+    if (repoName) {
+        getRepoIssues(repoName);
+        repoNameEl.textContent = repoName;
+    } else {
+        document.location.replace("./index.html");
+    }
+}
 
 var getRepoIssues = function (repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -7,14 +21,16 @@ var getRepoIssues = function (repo) {
         if (response.ok) {
             response.json().then(function (data) {
                 displayIssues(data);
-            })
+
+                if (response.headers.get("Link")) {
+                    displayWarning(repo);
+                }
+            });
         } else {
-            alert("There was a problem with your request!");
+            document.location.replace("./index.html");
         }
     });
 };
-
-getRepoIssues("facebook/react");
 
 var displayIssues = function (issues) {
     if (issues.length === 0) {
@@ -23,7 +39,7 @@ var displayIssues = function (issues) {
     }
     for (var i = 0; i < issues.length; i++) {
         var issueEl = document.createElement("a");
-        issueEl.classList = "llist-item flex-row justify-space-between align-center";
+        issueEl.classList = "list-item flex-row justify-space-between align-center";
         issueEl.setAttribute("href", issues[i].html_url);
         issueEl.setAttribute("target", "_blank");
 
@@ -44,3 +60,16 @@ var displayIssues = function (issues) {
         issueContainerEl.appendChild(issueEl);
     }
 }
+
+var displayWarning = function (repo) {
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+
+    limitWarningEl.appendChild(linkEl);
+};
+
+getRepoName();
